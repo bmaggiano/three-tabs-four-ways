69e8f981370f4ad15ccacdf75c6a1a5ad7ba5a02
bmaggiano <brandon.maggiano@gmail.com>
2023-06-16 10:55:57 -0700

    added header section to it's own component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import HeaderSection from "./components/headerSection";
 import ActiveSection from "./components/activeSection";
 import RenderSection from "./components/renderSection";
 import LocalStorageSection from "./components/localStorageSection";
@@ -8,14 +9,9 @@ export default function App() {
   return (
     <>
       <header>
-        <h1 className="title">
-          Three Tabs Three Ways<span className="underline"></span>
-        </h1>
-        <span className="title-subtext">
-          With vibrant popping colors that give{" "}
-          <span className="electric">"electric"</span> vibes!
-        </span>
-        <br />
+
+      <HeaderSection/>
+      
       </header>
 
       <main>
@@ -25,6 +21,7 @@ export default function App() {
         <RenderSection/>
 
         <LocalStorageSection/>
+
       </main>
     </>
   );
