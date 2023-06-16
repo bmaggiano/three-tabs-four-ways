008a2cb104a8b416596a17eee78056516100c5b2
bmaggiano <brandon.maggiano@gmail.com>
2023-06-16 11:03:22 -0700

    added comments to activeSection.js file describing functionality

--- src/components/activeSection.js
+++ src/components/activeSection.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import LightningIcon from "./lightningIcon"
+import LightningIcon from "./lightningIcon";
 import "../App.css";
 
 export default function ActiveSection() {
   const [active, setActive] = useState(0);
 
-  // this is our function for setting the index value for our "active" variable
+  // on click, set active variable to an index value
   const handleClick = (index) => {
     setActive(index);
   };
 
+  // array of messages that will be presented to user
   const messages = [
     "This is the message for tab 1",
     "This is the message for tab 2",
@@ -31,6 +32,8 @@ export default function ActiveSection() {
             </span>
             <br />
           </p>
+
+          {/* this code will be hidden until user hovers over description-container-one */}
           <p>
             <span className="hidden">
               // initialize state variable to 0
@@ -64,33 +67,38 @@ export default function ActiveSection() {
         <div className="tabs-container-one">
           <div className="button-container">
             <button
+              // onClick "active" variable will now have a value of 1
+              onClick={() => handleClick(1)}
+
+              // if active value is 1, class name will be "active-button left button", else it will have a class of "left-button" for styling
               className={
                 active === 1 ? "active-button left-button" : "left-button"
               }
-              onClick={() => handleClick(1)}
             >
               Tab 1
             </button>
             <button
+              onClick={() => handleClick(2)}
               className={
                 active === 2 ? "active-button middle-button" : "middle-button"
               }
-              onClick={() => handleClick(2)}
             >
               Tab 2
             </button>
             <button
+              onClick={() => handleClick(3)}
               className={
                 active === 3 ? "active-button right-button" : "right-button"
               }
-              onClick={() => handleClick(3)}
             >
               Tab 3
             </button>
           </div>
           <div className="message-box message-box-dark">
             <p>
+                {/* display message from messages array at the index of active minus 1... if active === 1, it will retrieve 0 index of array (message one) */}
               {messages[active - 1] ||
+            //   Or will display this message
                 "Click on the tabs to see different messages using the active method"}
             </p>
           </div>
--- src/components/headerSection.js
+++ src/components/headerSection.js
@@ -1,16 +1,16 @@
 import "../App.css";
 
 export default function HeaderSection() {
-    return(
-        <>
-                <h1 className="title">
-          Three Tabs Three Ways<span className="underline"></span>
-        </h1>
-        <span className="title-subtext">
-          With vibrant popping colors that give{" "}
-          <span className="electric">"electric"</span> vibes!
-        </span>
-        <br />
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">
+        Three Tabs Three Ways<span className="underline"></span>
+      </h1>
+      <span className="title-subtext">
+        With vibrant popping colors that give{" "}
+        <span className="electric">"electric"</span> vibes!
+      </span>
+      <br />
+    </>
+  );
 }
